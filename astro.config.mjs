0add28cf01b9842174622c73eda03f75f49385ab
somundane <98dana@gmail.com>
2026-09-10 14:38:19 -0400

    MXD, Reack, md + astro working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import { unified } from '@astrojs/markdown-remark';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  server: { port: 3000 },
+  integrations: [mdx(), react()],
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: 'append' }],
+        rehypeKatex,
+      ],
+    }),
+  },
 });
